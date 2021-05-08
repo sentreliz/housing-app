120d04c6f3d213328f3775af94577aac22753dde
Matthew Sessions <msessions@expediagroup.com>
2021-05-07 19:46:22 -0700

    added cleaner charts

--- app/src/components/ExampleChart.js
+++ app/src/components/ExampleChart.js
@@ -1,77 +1,61 @@
 import React, { useState, useEffect, useContext } from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+    CartesianGrid,
+    Legend,
+    Line,
+    LineChart,
+    Tooltip,
+    XAxis,
+    YAxis,
+    ResponsiveContainer,
+    Brush
+} from "recharts";
 
 
 
-const data = [
-    {
-        name: 'Page A',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
-    },
-    {
-        name: 'Page B',
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
-    },
-    {
-        name: 'Page C',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
-    },
-    {
-        name: 'Page D',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
-    },
-    {
-        name: 'Page E',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
-    },
-    {
-        name: 'Page F',
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
-    },
-    {
-        name: 'Page G',
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
-    },
-];
 const ExampleChart = () => {
 
-    return (
-        <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-                width={500}
-                height={300}
-                data={data}
-                margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
-        </ResponsiveContainer>
-    )
-}
 
+    const data = [
+        { date: "2055-6-1/1", name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
+        { date: "2055-6-1/2", name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
+        { date: "2055-6-1/3", name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
+        { date: "2055-6-1/4", name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
+        { date: "2055-6-1/5", name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
+        { date: "2055-6-1/6", name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
+        { date: "2055-6-1/7", name: "Page G", uv: 3490, pv: 4300, amt: 2100 },
+    ];
+    return (
+        <div>
+            <h2>useRechartToPng example with FileSaver</h2>
+            <br />
+            <ResponsiveContainer width="100%" height={400}>
+                <LineChart
+                    data={data}
+                    height={500}
+                    width={500}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 25 }}
+                >
+                    <XAxis dataKey="date" />
+
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Legend wrapperStyle={{ bottom: 5 }} />
+                    <Line
+                        dot={false}
+                        type="monotone"
+                        dataKey="pv"
+                        stroke="#8884d8"
+                        activeDot={{ r: 8 }}
+                    />
+                    <Line dot={false} type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    <Brush />
+                </LineChart>
+            </ResponsiveContainer>
+            <br />
+            <p>Source</p>
+
+        </div >
+    );
+};
 export default ExampleChart;
--- app/src/components/Single.js
+++ app/src/components/Single.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import MainChart from './MainChart';
 import ExampleChart from './ExampleChart';
+import ExampleReal from './ExampleReal';
 import { Grid, Paper, Container } from '@material-ui/core';
 
+const paper = {
+    backgroundColor: "white",
+    textAlign: "center",
+    padding: "1px",
+
+}
+
 const Single = ({ match }) => {
-    const paper = {
-        backgroundColor: "red",
+    // const [requestData, setRequestData] = useState({})
+    // const [chartData, setChartData] = useState([])
 
-        textAlign: "center",
-        padding: "20px",
+    // useEffect(() => {
+    //     const searchHousing = async () => {
+    //         const { data } = await axios.get(`https://sentreliz.com/housing/city/${match.params.id}`)
+    //         setRequestData(data.message)
+    //         const keys = Object.keys(data.message[0].pricing)
+    //         setChartKeys(keys)
+    //     }
+    //     searchHousing()
+    // }, []);
 
-    }
+    // useEffect(() => {
+    //     const dataHolder = []
+    //     dataOne.map((obj) => {
+    //         const holder = {
+    //             label: housingTypeMapping[obj.HouseType],
+    //             data: Object.values(obj.pricing),
+    //             fill: true
+    //         }
+    //         dataHolder.push(holder)
+    //     })
+    // }, [requestData])
 
     return (
         <Container>
@@ -20,7 +45,7 @@ const Single = ({ match }) => {
                     <Paper style={paper}>search</Paper>
                 </Grid>
                 <Grid item xs={12} md={8}>
-                    <Paper style={paper}><ExampleChart /></Paper>
+                    <Paper style={paper}><ExampleReal match={match} /></Paper>
                 </Grid>
                 <Grid item xs={6} md={3} lg={3}>
                     <Paper style={paper}>Recession recovery vs state</Paper>
